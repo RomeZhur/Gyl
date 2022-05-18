3d47975e318b534f2284b3cde0a64599c43d5b0b
RomeZhur <roman.edyardovich@gmail.com>
2022-05-18 21:08:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+i = 1000 + 7;
+audio = new Audio ("gul.mp3");
+
+function start () {
+    if (i < 0){
+        audio.pause()
+        i = 1000 + 7
+        clearInterval (timer);
+    }
+    i =i - 7
+    $("body").append("<h3>" + i + "</h3>" + "<br>")
+    $("html, body").animate({scrollTop:$(document).height() + $(window).height()}, 100)
+}
+$("#start").on("click", function (){
+    audio.play ();
+    let timer = setInterval(start, 250  )
+})
